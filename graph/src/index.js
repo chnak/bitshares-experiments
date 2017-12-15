b373630d00c20e06d8164ae0147247fd770237d6
Anton <anlopan@gmail.com>
2017-12-15 20:48:15 +0300

    Account balance, math pow at 10

--- graph/src/index.js
+++ graph/src/index.js
@@ -44,6 +44,9 @@ var api = {
 		    	reject(error);
 		    });
 		});
+	},
+	getBalance: (account_name) => {
+		e
 	}
 }
 
@@ -62,9 +65,9 @@ var helpers = {
 		let precision_diff = base.precision - quote.precision;
 
 		if (precision_diff > 0){
-			price = price / (precision_diff * 10);
+			price = price / Math.pow(10,precision_diff);
 		}else if(precision_diff < 0){
-			price = price * 10 * precision_diff;
+			price = price * Math.pow(10,precision_diff);
 		}
 		return Math.abs(price).toFixed(4);
 	},
@@ -77,40 +80,66 @@ var helpers = {
 }
 
 
-console.time("connect");
-console.time("all");
+
+function retrieveState(){
+	    api.fetchAssets(["BTS","OPEN.EOS","USD"]).then((result) => {
+	    	let [bts,eos,usd] = result;
+	    	let days_period = 7;
+	    	let full_day_bucket = 86400;
+	    	let hour_bucket = 3600;
+
+	    	Promise.all([
+	    		api.fetchStats(bts,usd,days_period,full_day_bucket),
+	    		api.fetchStats(bts,eos,days_period,hour_bucket),
+			]).then(([btsusd,btseos])=>{
+				//Calculate bts in usd per day costs
+				let usd_in_bts_days = [];
+				btsusd.forEach(function(bucket){
+	    			usd_in_bts_days.push(helpers.formatPrice(helpers.getTotalPrice(bucket),bts,usd));
+	    		});
+
+				let [startPrice,endPrice] = helpers.formatPrices(helpers.getStartEndPrices(btseos),bts,eos);
+
+				let startPriceUsd = startPrice / usd_in_bts_days[0];
+				let endPriceUSD = endPrice / usd_in_bts_days[usd_in_bts_days.length-1];
+				console.log("STARTEND ",startPrice,endPrice);
+				console.log("IN USD",startPriceUsd,endPriceUSD);
+				console.log("BTS CHANGE FOR ",days_period," days: ",startPrice / endPrice);
+				console.log("USD CHANGE FOR ",days_period," days: ",startPriceUsd / endPriceUSD);
+			});
+	    });    
+}
+
+function retrieveBalances(account_name){
+	Apis.instance().db_api().exec("get_full_accounts", [["anlopan364test2"], false])
+    .then( (results) => {
+    	let balances = results[0][1].balances;
+    	let active_balances = [];
+    	let balance_assets = [];
+    	balances.forEach(function(balance,index){
+    		if (balance.balance != 0){
+    			active_balances.push(balance);
+    			balance_assets.push(balance.asset_type);
+    		}
+    	});
+
+    	api.fetchAssets(balance_assets).then((result)=>{
+			active_balances.forEach((balance,i)=>{
+				active_balances[i].symbol = result[i].symbol
+				active_balances[i].precision = result[i].precision
+				active_balances[i].balanceReal = active_balances[i].balance / Math.pow(10,result[i].precision);
+			});
+    		console.log(active_balances);
+    		console.timeEnd("test");
+    	});
+    });
+}
+
+console.time("connect")
 api.connect().then((res) => {
 	console.timeEnd("connect");
-	console.time("requests");
-
-    api.fetchAssets(["BTS","OPEN.EOS","USD"]).then((result) => {
-    	let [bts,eos,usd] = result;
-    	let days_period = 7;
-    	let full_day_bucket = 86400;
-    	let hour_bucket = 3600;
-
-    	Promise.all([
-    		api.fetchStats(bts,usd,days_period,full_day_bucket),
-    		api.fetchStats(bts,eos,days_period,hour_bucket),
-		]).then((results)=>{
-			//Calculate bts in usd per day costs
-			let usd_in_bts_days = [];
-			results[0].forEach(function(bucket){
-    			usd_in_bts_days.push(helpers.formatPrice(helpers.getTotalPrice(bucket),bts,usd));
-    		});
-
-			let [startPrice,endPrice] = helpers.formatPrices(helpers.getStartEndPrices(results[1]),bts,eos);
-
-			let startPriceUsd = startPrice / usd_in_bts_days[0];
-			let endPriceUSD = endPrice / usd_in_bts_days[usd_in_bts_days.length-1];
-			console.log("STARTEND ",startPrice,endPrice);
-			console.log("IN USD",startPriceUsd,endPriceUSD);
-			console.log("BTS CHANGE FOR ",days_period," days: ",startPrice / endPrice);
-			console.log("USD CHANGE FOR ",days_period," days: ",startPriceUsd / endPriceUSD);
-			console.timeEnd("requests");
-			console.timeEnd("all");
-		});
-    });    
+	console.time("test");
+	retrieveBalances("trfnd");
 });
 
 

7a62280d711427cb8c7f15a71999aee9b2b3974b
Anton <anlopan@gmail.com>
2017-12-15 19:00:36 +0300

    Fin fetch EOS in USD stats via promise.all

--- graph/src/index.js
+++ graph/src/index.js
@@ -78,6 +78,7 @@ var helpers = {
 
 
 console.time("connect");
+console.time("all");
 api.connect().then((res) => {
 	console.timeEnd("connect");
 	console.time("requests");
@@ -87,36 +88,28 @@ api.connect().then((res) => {
     	let days_period = 7;
     	let full_day_bucket = 86400;
     	let hour_bucket = 3600;
-    	//console.log("ASSETS:",result)
 
-
-
-    	//console.log("Fetching infor for 7 day: ",base.symbol,quote.symbol);
-
-    	//get bts-usd stats for make array of USD price in BTS for days_period
-    	api.fetchStats(bts,usd,days_period,full_day_bucket).then((buckets)=>{
-    		let usd_in_bts_days = [];
-    		buckets.forEach(function(bucket){
+    	Promise.all([
+    		api.fetchStats(bts,usd,days_period,full_day_bucket),
+    		api.fetchStats(bts,eos,days_period,hour_bucket),
+		]).then((results)=>{
+			//Calculate bts in usd per day costs
+			let usd_in_bts_days = [];
+			results[0].forEach(function(bucket){
     			usd_in_bts_days.push(helpers.formatPrice(helpers.getTotalPrice(bucket),bts,usd));
     		});
 
-    		console.log("BTS IN USD",usd_in_bts_days);
+			let [startPrice,endPrice] = helpers.formatPrices(helpers.getStartEndPrices(results[1]),bts,eos);
 
-    		//Now we can calculate change in dollars for any asset
-    		api.fetchStats(bts,eos,days_period,hour_bucket).then((buckets)=>{
-    			let [startPrice,endPrice] = helpers.formatPrices(helpers.getStartEndPrices(buckets),bts,eos);
-    			let startPriceUsd = startPrice / usd_in_bts_days[0];
-    			let endPriceUSD = endPrice / usd_in_bts_days[usd_in_bts_days.length-1];
-    			console.log("STARTEND ",startPrice,endPrice);
-    			console.log("IN USD",startPriceUsd,endPriceUSD);
-    			console.log("BTS CHANGE FOR ",days_period," days: ",startPrice/endPrice);
-    			console.log("USD CHANGE FOR ",days_period," days: ",startPriceUsd/endPriceUSD);
-    			console.timeEnd("requests");
-    		});
-
-    		//console.log(helpers.formatPrices(helpers.getPrices(buckets),bts,usd));
-    		
-    	});    	
+			let startPriceUsd = startPrice / usd_in_bts_days[0];
+			let endPriceUSD = endPrice / usd_in_bts_days[usd_in_bts_days.length-1];
+			console.log("STARTEND ",startPrice,endPrice);
+			console.log("IN USD",startPriceUsd,endPriceUSD);
+			console.log("BTS CHANGE FOR ",days_period," days: ",startPrice / endPrice);
+			console.log("USD CHANGE FOR ",days_period," days: ",startPriceUsd / endPriceUSD);
+			console.timeEnd("requests");
+			console.timeEnd("all");
+		});
     });    
 });
 
